0f1bbdbbd43bb9107d97b37c3ac2c5ee6ce6d1ef
libresoftwaredistribution <libresoftwaredistribution@proton.me>
2023-05-11 16:56:49 -0300

    Fix

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -31,7 +31,7 @@ export default function Zigzag({title, subtitle, items}: ZigzagProps) {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width={540} height={405} quality={100} alt="Features 01" />
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={'https://libresoftdistro.com/images/features-03-image-01.png'} width={540} height={405} quality={100} alt="Features 01" />
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
