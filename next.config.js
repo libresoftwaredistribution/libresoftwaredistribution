3402637f7a6161ff0b8c9566717b012b943f82e4
libresoftwaredistribution <libresoftwaredistribution@proton.me>
2023-05-11 18:07:26 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,34 @@
 /** @type {import('next').NextConfig} */
+const optimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins');
+
 const isProd = true
+const pathPrefix = isProd ? 'https://libresoftdistro.com' : undefined
 
 const nextConfig = {
-  assetPrefix: isProd ? 'https://libresoftdistro.com' : undefined,
+  assetPrefix: pathPrefix,
   output: isProd ? 'export' : undefined,
   experimental: {
     appDir: true,
   },
+  env: {
+    pathPrefix,
+  },
 }
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 100,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/_next/static/images/',
+  }],
+  nextConfig
+]);
 
 module.exports = nextConfig
